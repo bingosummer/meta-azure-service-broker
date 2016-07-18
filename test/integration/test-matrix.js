af1c8511672d31f3d221dad9e169cb019bfe51bb
Bin Xia <binxi@microsoft.com>
2016-07-18 05:42:11 +0000

    Chnage the service name azurestorageblob to azure-storageblob

--- test/integration/test-matrix.js
+++ test/integration/test-matrix.js
@@ -10,7 +10,7 @@ bindingId = uuid.v4();
 resourceGroupName = 'cloud-foundry-' + instanceId;
 var storageAccountName = 'cf' + instanceId.replace(/-/g, '').slice(0, 22);
 var azurestorageblob = {
-  serviceName: 'azurestorageblob',
+  serviceName: 'azure-storageblob',
   serviceId: '2e2fc314-37b6-4587-8127-8f9ee8b33fea',
   planId: '6ddf6b41-fb60-4b70-af99-8ecc4896b3cf',
   instanceId: instanceId,
@@ -34,7 +34,7 @@ testMatrix.push(azurestorageblob);
 instanceId = uuid.v4();
 bindingId = uuid.v4();
 var azurestorageblobWithoutParameters = {
-  serviceName: 'azurestorageblob',
+  serviceName: 'azure-storageblob',
   serviceId: '2e2fc314-37b6-4587-8127-8f9ee8b33fea',
   planId: '6ddf6b41-fb60-4b70-af99-8ecc4896b3cf',
   instanceId: instanceId,
